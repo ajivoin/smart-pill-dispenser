2f229db705c64044c3b97e0f75d870087b377473
Alexander Shearer <sheareraws@gmail.com>
2019-11-02 17:11:04 -0400

    basic view

--- pill_server/app/controllers/schedule_controller.rb
+++ pill_server/app/controllers/schedule_controller.rb
@@ -1,4 +1,19 @@
 class ScheduleController < ActionController::Base
   def show
   end
+
+  def update
+    print params
+    @schedule = Schedule.find_by(id: params[:id])
+    @schedule.time = params[:'time(4i)'].to_i * 60 + params[:'time(5i)'].to_i
+    @schedule.day0 = params[:day0]
+    @schedule.day1 = params[:day1]
+    @schedule.day2 = params[:day2]
+    @schedule.day3 = params[:day3]
+    @schedule.day4 = params[:day4]
+    @schedule.day5 = params[:day5]
+    @schedule.day6 = params[:day6]
+    @schedule.save
+    redirect_to schedule_url()
+  end
 end
--- pill_server/config/routes.rb
+++ pill_server/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/schedule', to: 'schedule#show'
+  get '/schedule', to: 'schedule#show', as: 'schedule'
+  post '/schedule/:id', to: 'schedule#update'
 end
